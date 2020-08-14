9d33978f6555d79492a3c8df42de315375f4319f
MarioAlbertoNL1799 <1717110635@utectulancingo.edu.mx>
2020-08-14 12:01:17 -0500

    Editar menu de carrito compra

--- app/src/main/java/com/example/mktul_movil/CarritoCompras.java
+++ app/src/main/java/com/example/mktul_movil/CarritoCompras.java
@@ -2,7 +2,9 @@ package com.example.mktul_movil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CarritoCompras extends AppCompatActivity {
 
@@ -11,4 +13,24 @@ public class CarritoCompras extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrito_compras);
     }
+
+    public void Inicio(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    public void Categorias(View view){
+        Intent intent = new Intent(this, Categorias.class);
+        startActivity(intent);
+    }
+    public void Carrito(View view){
+        Intent intent = new Intent(this, CarritoCompras.class);
+        startActivity(intent);
+        finish();
+    }
+    public void Cuenta(View view){
+        Intent intent = new Intent(this, Config_Cuenta.class);
+        startActivity(intent);
+    }
+
 }
